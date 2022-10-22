0bd436369e248c557372c26833430f0c7f5c707b
Paulo Monezi <paulo.monezi@icloud.com>
2022-10-22 11:10:24 -0400

    testing new deploy settings

--- src/components/buttons/button/button.js
+++ src/components/buttons/button/button.js
@@ -1,20 +1,5 @@
 import './button.css'
 
-// const LabelName = (props) => {
-//     alert('This label name is ' + props.label)
-// }
-
-// const Button = (props) => {
-//     return <>
-//         <button className='btn' onClick={() => { LabelName(props) }}>{props.label}</button>
-//     </>
-
-// }
-
-// Button.defaultProps = {
-//     label: 'Click Here'
-// }
-
 const Button = ({ label = 'Click Here' }) => {
 
     function showLabel(label) {
